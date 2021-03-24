9d840d3c48d6458977af4218142aad6b48017e25
rag-hav <raghav.ag13@gmail.com>
2021-03-24 19:04:28 +0530

    prelims

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,35 +1,71 @@
-function makeTimer() {
+function makeTimer1() {
 
-  var endTime = new Date("April 11, 2021 05:30:00 UTC");
-  var endTime = (Date.parse(endTime)) / 1000;
+        var endTime = new Date("March 24, 2021 16:30:00 UTC");
+        var endTime = (Date.parse(endTime)) / 1000;
 
-  var now = new Date();
-  var now = (Date.parse(now) / 1000);
+        var now = new Date();
+        var now = (Date.parse(now) / 1000);
 
-  var timeLeft = endTime - now;
+        var timeLeft = endTime - now;
 
-  var days = Math.floor(timeLeft / 86400);
-  var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-  var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
-  var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+        var days = Math.floor(timeLeft / 86400);
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
 
-  if (hours < "10") {
-    hours = "0" + hours;
-  }
-  if (minutes < "10") {
-    minutes = "0" + minutes;
-  }
-  if (seconds < "10") {
-    seconds = "0" + seconds;
-  }
+        if (hours < "10") {
+                hours = "0" + hours;
+        }
+        if (minutes < "10") {
+                minutes = "0" + minutes;
+        }
+        if (seconds < "10") {
+                seconds = "0" + seconds;
+        }
 
-  $("#days").html(days + '<span class="timerSpan" >Days</span>');
-  $("#hours").html(hours + '<span class="timerSpan" >Hours</span>');
-  $("#minutes").html(minutes + '<span class="timerSpan" >Minutes</span>');
-  $("#seconds").html(seconds + '<span class="timerSpan" >Seconds</span>');
+        $("#daysPre").html(days + '<span class="timerSpanPre" >Days</span>');
+        $("#hoursPre").html(hours + '<span class="timerSpanPre" >Hours</span>');
+        $("#minutesPre").html(minutes + '<span class="timerSpanPre" >Minutes</span>');
+        $("#secondsPre").html(seconds + '<span class="timerSpanPre" >Seconds</span>');
 
 }
 
 setInterval(function() {
-  makeTimer();
+        makeTimer1();
+}, 1000);
+
+function makeTimer() {
+
+        var endTime = new Date("April 11, 2021 05:30:00 UTC");
+        var endTime = (Date.parse(endTime)) / 1000;
+
+        var now = new Date();
+        var now = (Date.parse(now) / 1000);
+
+        var timeLeft = endTime - now;
+
+        var days = Math.floor(timeLeft / 86400);
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+        if (hours < "10") {
+                hours = "0" + hours;
+        }
+        if (minutes < "10") {
+                minutes = "0" + minutes;
+        }
+        if (seconds < "10") {
+                seconds = "0" + seconds;
+        }
+
+        $("#daysFinal").html(days + '<span class="timerSpan" >Days</span>');
+        $("#hoursFinal").html(hours + '<span class="timerSpan" >Hours</span>');
+        $("#minutesFinal").html(minutes + '<span class="timerSpan" >Minutes</span>');
+        $("#secondsFinal").html(seconds + '<span class="timerSpan" >Seconds</span>');
+
+}
+
+setInterval(function() {
+        makeTimer();
 }, 1000);
